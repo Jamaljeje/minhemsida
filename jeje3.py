3c672a47948fcdb6ff05c64c123c32f5838c9728
Jamal Jeje <jamaljeje22@gmail.com>
2025-04-01 02:38:44 +0200

    Update jeje3.py with new image file names

--- jeje3.py
+++ jeje3.py
@@ -3,7 +3,17 @@ import os
 
 app = Flask(__name__)
 
-styles = [{'id': 1, 'name': 'Classic Fade', 'price': 200, 'image': 'style1.jpg', 'gender': 'men'}, {'id': 2, 'name': 'Beard Trim', 'price': 250, 'image': 'style2.jpg', 'gender': 'men'}, {'id': 3, 'name': 'Oil & Color', 'price': 300, 'image': 'style3.jpg', 'gender': 'men'}, {'id': 4, 'name': 'Elegant Braids', 'price': 500, 'image': 'style4.jpg', 'gender': 'women'}, {'id': 5, 'name': 'Taper Fade', 'price': 250, 'image': 'style5.jpg', 'gender': 'women'}, {'id': 6, 'name': 'Bold Afro', 'price': 300, 'image': 'style6.jpg', 'gender': 'women'}, {'id': 7, 'name': 'Undercut Long Hair', 'price': 400, 'image': 'undercut_long.jpg', 'gender': 'men'}, {'id': 8, 'name': 'Beard Trim', 'price': 350, 'image': 'style8.jpg', 'gender': 'men'}, {'id': 9, 'name': 'Low Fade', 'price': 220, 'image': 'style9.jpg', 'gender': 'men'}]
+styles = [
+    {'id': 1, 'name': 'Classic Fade', 'price': 200, 'image': 'Classic Fade.jpg', 'gender': 'men'},
+    {'id': 2, 'name': 'Beard Trim', 'price': 250, 'image': 'Beard Trim.jpg', 'gender': 'men'},
+    {'id': 3, 'name': 'Oil & Color', 'price': 300, 'image': 'Oil & Color.jpg', 'gender': 'men'},
+    {'id': 4, 'name': 'Elegant Braids', 'price': 500, 'image': 'Elegant Braids.jpg', 'gender': 'women'},
+    {'id': 5, 'name': 'Taper Fade', 'price': 250, 'image': 'Taper Fade.jpg', 'gender': 'men'},
+    {'id': 6, 'name': 'Bold Afro', 'price': 300, 'image': 'Bold Afro.jpg', 'gender': 'men'},
+    {'id': 7, 'name': 'Undercut Long Hair', 'price': 400, 'image': 'Undercut Long Hair.jpg', 'gender': 'men'},
+    {'id': 8, 'name': 'Beard Trim', 'price': 350, 'image': 'Beard Trim.jpg', 'gender': 'men'},
+    {'id': 9, 'name': 'Low Fade', 'price': 220, 'image': 'Low Fade.jpg', 'gender': 'men'}
+]
 
 @app.route('/')
 def index():
